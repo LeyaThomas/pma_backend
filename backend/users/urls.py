7eb45f41257a0d5130b102eac9a46c641f44fa7e
LeyaThomas <leyathomas1352002@gmail.com>
2024-04-30 21:48:01 +0530

    Dashboard userdetails

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 #users.urls.py
 from django.urls import path
-from .views import CompanyListView
+from .views import CompanyListView, UserDetailView
 
 urlpatterns = [
     path('companies/', CompanyListView.as_view(), name='companylist'),
+    path('userdetail/<int:pk>/', UserDetailView.as_view(), name='userdetail'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,28 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Company
-from .serializers import CompanySerializer
+from .models import Company, Cususer
+from .serializers import CompanySerializer, CusUserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
+from django.http import Http404
 
 # Create your views here.
 @permission_classes([AllowAny])
 class CompanyListView(generics.ListAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+@permission_classes([AllowAny])
+class UserDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Cususer.objects.get(pk=pk)
+        except Cususer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        cususer = self.get_object(pk)
+        serializer = CusUserSerializer(cususer)
+        return Response(serializer.data)
