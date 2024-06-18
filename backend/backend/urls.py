e244198bc43bb90b5902040168217be35e74f08c
LeyaThomas <leyathomas1352002@gmail.com>
2024-06-18 19:38:29 +0530

    root path added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
