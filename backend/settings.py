e98b5def8e827c86f438a21bc8dc027ab031b40b
LeyaThomas <leyathomas1352002@gmail.com>
2024-09-20 18:47:49 +0530

    *

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ky$^*i7mdftm3(#+25@%g-d-%cv1!yy!$nk!bg3meo$f&39#y6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
