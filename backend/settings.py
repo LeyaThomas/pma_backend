de0f2a0c7edfb7f8be08cb35791eb5575ca7a081
LeyaThomas <leyathomas1352002@gmail.com>
2024-09-20 19:04:48 +0530

    .

--- backend/settings.py
+++ backend/settings.py
@@ -159,10 +159,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+
+       'http://localhost:3000',
+
+)
+
+
+
 CORS_ALLOW_CREDENTIALS = True
 
 MEDIA_URL = '/media/'
